f4b0ceac77cd40d310594434c9e2f6ef0a532d6e
Iago Schaffel <iago.schaffel@hotmail.com>
2020-04-03 12:03:53 -0300

    Começo da implementaçao de Tabuleiro e Peça2

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,14 +1,13 @@
 package aplicacao;
 
 import jogotabuleiro.Posicao;
+import jogotabuleiro.Tabuleiro;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Posicao pos = new Posicao(3, 5);
- 		
-		System.out.println(pos);
+		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
 	}
 
