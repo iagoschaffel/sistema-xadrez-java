944fde9781c2b1522be489b3079279562358d203
Iago Schaffel <iago.schaffel@hotmail.com>
2020-04-03 12:02:26 -0300

    Começo da implementaçao de Tabuleiro e Peça

---
+++ src/jogotabuleiro/Peça.java
@@ -0,0 +1,22 @@
+package jogotabuleiro;
+
+public class Peça {
+	
+	protected Posicao posicao;
+	
+	private Tabuleiro tabuleiro;
+
+	public Peça(Tabuleiro tabuleiro) {
+		this.tabuleiro = tabuleiro;
+		posicao = null;
+	}
+
+	protected Tabuleiro getTabuleiro() {
+		return tabuleiro;
+	}
+
+	
+	
+	
+
+}
