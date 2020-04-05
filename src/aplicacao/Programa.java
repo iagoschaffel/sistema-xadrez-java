81d814943ccd5bf0dd5f586aa133d2880f12c379
Iago Schaffel <iago.schaffel@hotmail.com>
2020-04-05 11:57:02 -0300

    Camada de Xadrez e imprimindo o tabuleiro

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,13 +1,14 @@
 package aplicacao;
 
-import jogotabuleiro.Posicao;
-import jogotabuleiro.Tabuleiro;
+import xadrez.PartidaXadrez;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+		PartidaXadrez partidaxadrez = new PartidaXadrez();
+		
+		UI.printTabuleiro(partidaxadrez.getPeças());
 
 	}
 
---
+++ src/xadrez/PartidaXadrez.java
@@ -0,0 +1,24 @@
+package xadrez;
+
+import jogotabuleiro.Tabuleiro;
+
+public class PartidaXadrez {
+	
+	private Tabuleiro tabuleiro;
+	
+	public PartidaXadrez() {
+		tabuleiro = new Tabuleiro(8,8);
+	}
+	
+	public PeçaXadrez[][] getPeças() {
+		
+		PeçaXadrez[][] mat = new PeçaXadrez[tabuleiro.getLinhas()][tabuleiro.getColunas()];
+		for(int i=0;i<tabuleiro.getLinhas();i++) {
+			for(int j=0;j<tabuleiro.getColunas();j++) {
+				mat[i][j] = (PeçaXadrez) tabuleiro.peça(i,j);
+			}
+		}
+		return mat;
+	}
+
+}
